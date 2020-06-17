3f1651f8b3904b73e9cf9d75c6070e965410c31b
George Caesar <giorgio.cesaroni@me.com>
2020-06-17 12:29:51 -0400

    Initial commit

---
+++ js/clock.js
@@ -0,0 +1,38 @@
+const hourHand = document.getElementById('hour-hand');
+const minuteHand = document.getElementById('minute-hand');
+const secondHand = document.getElementById('second-hand');
+const background = document.body;
+const search = document.getElementById("search");
+const welcome = document.getElementById("welcome");
+
+function runClock() {
+  const now = new Date();
+  hourHand.style.transform = `rotate(${(now.getHours() % 12) * 30 + (now.getMinutes() / 2)}deg)`;
+  minuteHand.style.transform = `rotate(${now.getMinutes() * 6 + (now.getSeconds() / 10)}deg)`;
+  secondHand.style.transform = `rotate(${now.getSeconds() * 6 + (now.getMilliseconds() / (1000 / 6))}deg)`;
+  setTimeout(() => {
+    requestAnimationFrame(runClock);
+  }, 1000);
+}
+
+function salute() {
+  const now = new Date();
+  if (now.getHours() < 12) {
+    welcome.innerHTML = 'Good morning.';
+  } else if (now.getHours() >= 12 && now.getHours() < 18) {
+    welcome.innerHTML = 'Good afternoon.';
+  } else {
+    welcome.innerHTML = 'Good afternoon.';
+  }
+}
+
+runClock();
+salute();
+
+document.addEventListener('keydown', e => {
+  if (e.key == "Enter") {
+    window.open(`http://www.google.com/search?q=${search.value}`);
+    search.value = '';
+  }
+  search.focus();
+})
